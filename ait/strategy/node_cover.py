e1daedd623120315fbea32d45a02cc6fb6cb88ae
Josh Chen <bochfchen.code@gmail.com>
2024-03-12 00:29:25 -0400

    add node coverage

---
+++ ait/strategy/node_cover.py
@@ -0,0 +1,29 @@
+"""
+Node coverage
+Covers all the nodes (states)
+"""
+
+import logging
+
+from ait.graph_wrapper import GraphWrapper, Arrow
+from ait.strategy.strategy import Strategy
+
+
+class NodeCover(Strategy):
+    def __init__(self):
+        self._paths: list[list[Arrow]] = []
+
+    def travel(self, graph_wrapper: GraphWrapper, start: str):
+        """
+        depth first search on a graph, start from the initial vertex of the
+        graph, visit each vertex at least once
+
+        :param graph_wrapper: the graph
+        :type graph_wrapper: GraphWrapper
+        :param start: the initial state name of the graph
+        :type start: str
+        """
+        graph = graph_wrapper.graph
+        paths = graph.get_all_shortest_paths(start)
+        for path in paths:
+            logging.info("shortest path from %s: %s", start, path)
